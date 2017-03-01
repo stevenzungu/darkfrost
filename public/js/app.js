1232ad3bfd4c679959fb7dcbf70fca78306e295f
Mac <Mac@Macs-Air.itpro.tv>
2017-02-28 20:13:26 -0500

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,32 @@ var currentlyWidget = new Vue({
     precipProbability: 0.30,
     humidity: 0.61,
     location: 'Gainesville, FL'
+  },
+  methods: {
+    iconUrl: function(iconString){
+      return `/images/${iconString}.png`;
+    }
+  },
+  created: function(){
+    axios.get('/weather/29.1,-81.4')
+        .then(function(response){
+          var data = response.data.currently;
+          currentlyWidget.time = data.time;
+          currentlyWidget.summary = data.summary;
+          currentlyWidget.icon = data.icon;
+          currentlyWidget.apparentTemperature = data.apparentTemperature;
+          currentlyWidget.precipProbability = data.precipProbability;
+          currentlyWidget.humidity = data.humidity;
+        })
+        .catch(function(err){
+          console.log(err);
+        });
   }
 });
 
-////
+
+
+
+
+
+//
